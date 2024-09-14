38d10b953e3a28ebe88beccc7b1e6763718195bb
realkhoa <khoa.nguyenba@outlook.com>
2024-09-14 14:04:42 +0700

    transpile package

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  esmExternals: "loose",
+  transpilePackages: [ "antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table" ],
 };
 
 export default nextConfig;
