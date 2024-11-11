2eed9b1d051cfd2c16d26c15332993b59d961d65
Tran Nguyen Quoc Bao <122146748+tnqbao@users.noreply.github.com>
2024-11-11 22:39:01 +0700

    add auto build script

--- start.sh
+++ start.sh
@@ -2,6 +2,11 @@
 
 git pull origin main
 
-docker stop -f blog_frontend
-docker run -d -p 3005:3005 blog_frontend
+docker stop blog_frontend || true
+docker rm blog_frontend || true
+
+docker build -t blog_frontend .
+
+docker run -d --name blog_frontend -p 3005:3005 blog_frontend
+
 docker system prune -a --volumes -f
