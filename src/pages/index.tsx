8acdd371985ce53b1ae3a1107ff92574bd10efb5
Tran Nguyen Quoc Bao <122146748+tnqbao@users.noreply.github.com>
2024-11-28 18:51:21 +0700

    add: Voting Button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import {GetServerSideProps} from 'next';
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
-import {Button, Layout} from 'antd';
 import nextI18NextConfig from '../../next-i18next.config.js';
 import {useRouter} from "next/router";
 import {useTranslation} from "next-i18next";
 
-const {Content} = Layout;
 
 
 const HomePage = () => {
